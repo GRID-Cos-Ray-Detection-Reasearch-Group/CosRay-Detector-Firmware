6d9bb1574843a0f0e9802ac3cbced89bbcc1263b
soranatsu <soranoyamiyou@163.com>
2025-11-23 19:37:42 +0800

    Update typedefs.h

--- BSP/typedefs.h
+++ BSP/typedefs.h
@@ -65,4 +65,22 @@ typedef struct {
 
 typedef uint8_t DataMessage_t; // TODO 检查消息类型
 
+
+#pragma pack(push, 1)
+typedef struct {
+    uint16_t header;        // 帧头 (固定为 PACKET_HEADER = 0xAA55)
+    uint8_t cmd_type;       // 指令类型 
+    uint8_t param_len;      // 参数长度 (0 ~ (RX_BUFFER_SIZE - PACKET_OVERHEAD))
+    uint8_t params[];       // 可变长度参数 
+} CommandFrameHeader_t;
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+typedef struct {
+    CommandFrameHeader_t header;  // 命令帧头部
+    uint16_t checksum;            // 校验和 
+    uint16_t trailer;             // 帧尾 (固定为 PACKET_TRAILER = 0x55AA)
+} CommandFrame_t;
+#pragma pack(pop)
+
 #endif // TYPEDEFS_H
